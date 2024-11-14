31abdd0428a7c1d84e3d2d2e8cd32d825c53bdf9
ZulfikarAkbar06 <pekargans@gmail.com>
2024-11-14 20:38:39 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from matematika import *
+
+print (f"nilai dari pi adalah {pi}")
+
+jari_jari = int(input("Masukkan nilai jari jari :"))
+print (f"luas lingkaran : {luas_lingkaran(jari_jari)}")
+
+sisi = int(input("Masukkan panjang sisi "))
+print (f"luas persegi : {luas_persegi(sisi)}")
+
+print(f"nilai dari akar {angka} adalah {akar_kuadrat}")
+print(f"nilai dari log {log_angka} adalah {logaritma}")
+print(f"nilai sin dari {sudut_derajat} adalah {sinus}")
+print(f"nilai dari angka {angka_faktor} faktorial adalah {faktorial}")
+print(f"Nilai eksponen e^{x} adalah: {exp_x}")
