c4949900f171187e2d00c6e3c4be44c70e5d8b40
ZulfikarAkbar06 <pekargans@gmail.com>
2024-11-14 20:37:11 +0700

    Create matematika.py

---
+++ matematika.py
@@ -0,0 +1,29 @@
+import math as math
+
+pi = math.pi
+
+def luas_lingkaran(n):
+    hasil = pi * n * n
+    return hasil
+
+def luas_persegi(n):
+    hasil = n * n
+    return hasil
+
+angka = 6
+akar_kuadrat = math.sqrt(angka)
+
+log_angka = 2006
+logaritma = math.log10(log_angka)
+
+sudut_derajat = 20
+sudut_radian = math.radians(sudut_derajat)
+sinus = math.sin(sudut_radian)
+
+angka_faktor = 5
+faktorial = math.factorial(angka_faktor)
+
+x = 2.5
+exp_x = math.exp(x)
+
+
